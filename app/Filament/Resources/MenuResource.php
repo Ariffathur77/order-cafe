14d7e65efac770a32d48cdd4cc6ec20df3dddd29
Ariffathur77 <ariffathur858@gmail.com>
2025-08-09 17:46:33 +0700

    update ui

--- app/Filament/Resources/MenuResource.php
+++ app/Filament/Resources/MenuResource.php
@@ -22,7 +22,8 @@ class MenuResource extends Resource
 {
     protected static ?string $model = Menu::class;
 
-    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+    protected static ?string $navigationIcon = 'heroicon-o-shopping-cart';
+    protected static ?int $navigationSort = 2;
 
     public static function form(Form $form): Form
     {
